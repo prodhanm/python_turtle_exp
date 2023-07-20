43f8bf13e6bfaba584798aae5d4c384dd2a422c5
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-20 11:12:32 -0400

    added another square to create a partial overlap.

--- overlap.py
+++ overlap.py
@@ -1,23 +1,46 @@
 import turtle
 import tkinter as tk
 
-def square():
+def square(x,y):
     turtle.color("black")
 
     turtle.penup()
-    turtle.goto(100, -120)
+    turtle.goto(x,y)
     turtle.pendown()
 
     for pen in range(4):
         turtle.left(90)
-        turtle.forward(260)
+        turtle.forward(200)
     
-square()
+square(150,-120)
 
-def round():
+def round(x):
     turtle.color("blue")
-    turtle.circle(260)
+    turtle.circle(x)
+
+round(200)
+square(350,80)
+
+def triang(x,y):
+    turtle.color("red")
+    
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+
+    #for pen in range(3):
+    #    turtle.left(45)
+    #    turtle.forward(100)
+    # used for a three side hex shape
+
+    for pen in range(2):
+        turtle.left(90)
+        turtle.forward(200)
+
+    turtle.left(135)
+    turtle.forward(280)
 
     turtle.exitonclick()
 
-round()
+triang(150,-120)
+#triang(120,-150)
